da1517709cc2adc3c5f3a5b7e19a2a0a6f5f850c
bensmus <bensmus@gmail.com>
2023-12-25 16:25:58 -0800

    variable changes

--- discrete_hillclimb.py
+++ discrete_hillclimb.py
@@ -4,36 +4,37 @@ import random
 
 def generate_random_assignment(worker_time: int, ride_times: Dict[str, int], can_check: Dict[str, Set[str]]) -> Tuple[Dict[str, str], Dict[str, int]]:
     
-    def dfs(p_assignment: Dict[str, str], p_worker_times_remaining: Dict[str, int]) -> Tuple[Dict[str, str], Dict[str, int]]:
+    def dfs(partial_assignment: Dict[str, str], partial_worker_times_remaining: Dict[str, int]) -> Tuple[Dict[str, str], Dict[str, int]]:
         """
-        p_assignment: Dictionary of {ride: worker...} containing a subset of rides (partial assignment).
-        c_... means complete assignment or worker times remaining.
+        partial_assignment: Dictionary of {ride: worker...} containing a subset of rides.
         worker_times: How much time does every worker have remaining.
         Randomized dfs: find a random solution to CSP that is not even locally optimal.
         """
         # Base case: 
-        if len(p_assignment) == len(ride_times):
-            return p_assignment, p_worker_times_remaining
+        if len(partial_assignment) == len(ride_times):
+            return partial_assignment, partial_worker_times_remaining
         # Recursive case:
-        ride = next(ride for ride in ride_times if ride not in p_assignment)
+        ride = next(ride for ride in ride_times if ride not in partial_assignment)
         workers = list(can_check.keys())
         random.shuffle(workers)
         for worker in workers: # Try to assign every worker to the ride in a random order.
-            if ride in can_check[worker] and ride_times[ride] <= p_worker_times_remaining[worker]:
+            if ride in can_check[worker] and ride_times[ride] <= partial_worker_times_remaining[worker]:
                 # Recursive call, adding ride and worker to partial_assignment, worker_times reflect remaining time.
-                c_assignment, c_worker_times_remaining = dfs(
-                    p_assignment | {ride: worker}, 
-                    p_worker_times_remaining | {worker: p_worker_times_remaining[worker] - ride_times[ride]}
+                complete_assignment, complete_worker_times_remaining = dfs(
+                    partial_assignment | {ride: worker}, 
+                    partial_worker_times_remaining | {worker: partial_worker_times_remaining[worker] - ride_times[ride]}
                 )
-                if c_assignment:
-                    return c_assignment, c_worker_times_remaining
+                if complete_assignment:
+                    return complete_assignment, complete_worker_times_remaining
         # Could not find a complete assignment based on the partial_assignment and worker_times_remaining.
-        return {}, p_worker_times_remaining
+        return {}, partial_worker_times_remaining
     
-    c_assignment, c_worker_times_remaining = dfs({}, {worker: worker_time for worker in can_check})
-    if c_assignment == {}:
+    complete_assignment, complete_worker_times_remaining = dfs({}, {worker: worker_time for worker in can_check})
+    if complete_assignment == {}:
         raise Exception("No assignment exists, problem is overconstrained.")
-    return c_assignment, c_worker_times_remaining
+    
+    # Now hillclimb this assignment
+    return complete_assignment, complete_worker_times_remaining
 
 
 if __name__ == '__main__':
