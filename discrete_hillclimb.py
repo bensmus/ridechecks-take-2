601878b7dfdb99b76263863a8d04c29407b8d185
bensmus <bensmus@gmail.com>
2023-12-26 02:12:46 -0800

    improved tests

--- discrete_hillclimb.py
+++ discrete_hillclimb.py
@@ -83,17 +83,22 @@ def generate_random_assignment(worker_time: int, ride_times: Dict[str, int], can
 
 
 if __name__ == '__main__':
-    worker_time = 40
+    worker_time = 30
     ride_times = {
         'RA': 10, 
         'RB': 12, 
         'RC': 14, 
         'RD': 15, 
-        'RE': 20
+        'RE': 20,
+        'RF': 1,
+        'RG': 4,
+        'RH': 11
     }
     can_check = {
         'WA': {'RA', 'RB'},
         'WB': {'RA', 'RB', 'RC', 'RD', 'RE'},
-        'WC': {'RE', 'RD'}
+        'WC': {'RE', 'RD', 'RA'},
+        'WD': {'RG', 'RF', 'RC'},
+        'WE': {'RH', 'RA', 'RD'}
     }
     print(generate_random_assignment(worker_time, ride_times, can_check))
